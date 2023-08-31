23772196d048a819183e930480bf8908f8455dc3
CTMobileDev <smallscreensw@hotmail.com>
2023-08-31 00:27:57 -0500

    After first Android run

--- CarLog/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/CarLog.AssemblyInfo.cs
+++ CarLog/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/CarLog.AssemblyInfo.cs
@@ -11,10 +11,6 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "78ba5315-bf0f-4e05-994b-5e0ff4105bcf")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "CarLog")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarLog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
