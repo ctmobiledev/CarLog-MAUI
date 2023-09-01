304f8e4888acce14dec079449ae9300fae621c10
CTMobileDev <smallscreensw@hotmail.com>
2023-09-01 16:44:01 -0500

    Updates to repository

--- CarLog/Repository/CLRepository.cs
+++ CarLog/Repository/CLRepository.cs
@@ -13,49 +13,126 @@ namespace CarLog.Repository
 
         public static List<Vehicle> Vehicles = new List<Vehicle>();
 
-        public static void LoadData()
+        public async static Task LoadDataAsync()
         {
 
-            // to create one
-            var testGuid = Guid.NewGuid();
-            Debug.WriteLine(">>> testGuid = " + testGuid.ToString());
+            await Task.Run(() => {
 
-            CLRepository.Vehicles.Add(new Vehicle
-            {
-                VID = new Guid("e57b36ee-5abd-465c-ab8c-87d3cca3545c"),
-                VehicleYear = 2017,
-                VehicleMaker = "Chevrolet",
-                VehicleModel = "Spark",
-                VehicleColor = "Splash",
-                VehicleMileage = 175000,
-                LicensePlateTag = "WIN123",
-                LicensePlateState = "TX",
-                LicensePlateExpiry = "2023-12-31"
+                // to create one
+                var testGuid = Guid.NewGuid();
+                Debug.WriteLine(">>> testGuid = " + testGuid.ToString());
+
+                CLRepository.Vehicles.Add(new Vehicle
+                {
+                    VID = new Guid("e57b36ee-5abd-465c-ab8c-87d3cca3545c"),
+                    VehicleYear = 2017,
+                    VehicleMaker = "Chevrolet",
+                    VehicleModel = "Spark",
+                    VehicleColor = "Splash",
+                    VehicleMileage = 175000,
+                    LicensePlateTag = "WIN123",
+                    LicensePlateState = "TX",
+                    LicensePlateExpiry = "2023-12-31",
+                    VehicleEvents = new List<VehicleEvent>
+                {
+                    new VehicleEvent
+                    {
+                        VID = new Guid("e57b36ee-5abd-465c-ab8c-87d3cca3545c"),
+                        EventId = new Guid("00000001-5abd-465c-ab8c-87d3cca3545c"),
+                        EventMileage = 100000,
+                        EventName = "Oil Change",
+                        EventTimestamp = DateTime.Now
+                    },
+                    new VehicleEvent
+                    {
+                        VID = new Guid("e57b36ee-5abd-465c-ab8c-87d3cca3545c"),
+                        EventId = new Guid("00000002-5abd-465c-ab8c-87d3cca3545c"),
+                        EventMileage = 112000,
+                        EventName = "Radiator Flush",
+                        EventTimestamp = DateTime.Now
+                    },
+                    new VehicleEvent
+                    {
+                        VID = new Guid("e57b36ee-5abd-465c-ab8c-87d3cca3545c"),
+                        EventId = new Guid("00000003-5abd-465c-ab8c-87d3cca3545c"),
+                        EventMileage = 117000,
+                        EventName = "New Tires",
+                        EventTimestamp = DateTime.Now
+                    },
+                }
+                });
+
+                CLRepository.Vehicles.Add(new Vehicle
+                {
+                    VID = new Guid("a47b36ee-5abd-465c-ab8c-87d3cca3545c"),
+                    VehicleYear = 2024,
+                    VehicleMaker = "Chevrolet",
+                    VehicleModel = "Malibu",
+                    VehicleColor = "Gold",
+                    VehicleMileage = 28000,
+                    LicensePlateTag = "WIN123",
+                    LicensePlateState = "TX",
+                    LicensePlateExpiry = "2023-12-31"
+                });
+
+                CLRepository.Vehicles.Add(new Vehicle
+                {
+                    VID = new Guid("881b36ee-5abd-465c-ab8c-87d3cca3545c"),
+                    VehicleYear = 2023,
+                    VehicleMaker = "Kia",
+                    VehicleModel = "Forte",
+                    VehicleColor = "Black",
+                    VehicleMileage = 18400,
+                    LicensePlateTag = "YES183",
+                    LicensePlateState = "TX",
+                    LicensePlateExpiry = "2023-12-31"
+                });
+
             });
-            CLRepository.Vehicles.Add(new Vehicle
-            {
-                VID = new Guid("a47b36ee-5abd-465c-ab8c-87d3cca3545c"),
-                VehicleYear = 2024,
-                VehicleMaker = "Chevrolet",
-                VehicleModel = "Malibu",
-                VehicleColor = "Gold",
-                VehicleMileage = 28000,
-                LicensePlateTag = "WIN123",
-                LicensePlateState = "TX",
-                LicensePlateExpiry = "2023-12-31"
-            });
-            CLRepository.Vehicles.Add(new Vehicle
-            {
-                VID = new Guid("881b36ee-5abd-465c-ab8c-87d3cca3545c"),
-                VehicleYear = 2023,
-                VehicleMaker = "Kia",
-                VehicleModel = "Forte",
-                VehicleColor = "Black",
-                VehicleMileage = 18400,
-                LicensePlateTag = "YES183",
-                LicensePlateState = "TX",
-                LicensePlateExpiry = "2023-12-31"
-            });
+
         }
+
+        public async static Task AddVehicleAsync()
+        {
+
+            Debug.WriteLine(">>> AddVehicleAsync fired");
+
+            await Task.Run(() =>
+            {
+                CLRepository.Vehicles.Add(new Vehicle
+                {
+                    VID = Guid.NewGuid(),
+                    VehicleYear = 2023,
+                    VehicleMaker = "Ford",
+                    VehicleModel = "Fiesta",
+                    VehicleColor = "Green",
+                    VehicleMileage = 25000,
+                    LicensePlateTag = "FRD192",
+                    LicensePlateState = "TX",
+                    LicensePlateExpiry = "2023-11-30"
+                });
+            });
+
+        }
+
+        public async static Task AddVehicleEventAsync(Vehicle veh)
+        {
+
+            Debug.WriteLine(">>> AddVehicleEventAsync fired");
+
+            await Task.Run(() =>
+            {
+                veh.VehicleEvents.Add(new VehicleEvent
+                {
+                    VID = new Guid("e57b36ee-5abd-465c-ab8c-87d3cca3545c"),
+                    EventId = new Guid("00000001-5abd-465c-ab8c-87d3cca3545c"),
+                    EventMileage = 100000,
+                    EventName = "Oil Change",
+                    EventTimestamp = DateTime.Now
+                });
+            });
+
+        }
+
     }
 }
