46a5e071cc08999425dae5aad81c3c179d5e9f00
CTMobileDev <smallscreensw@hotmail.com>
2023-09-11 18:28:32 -0500

    Addition Of Custom Logo, Realm Libraries

---
+++ CarLog/ModelsRealm/VehicleEventRealm.cs
@@ -0,0 +1,35 @@
+﻿using Realms;
+using Realms.Schema;
+using Realms.Weaving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLog.ModelsRealm
+{
+    public partial class VehicleEventRealm : IRealmObject
+    {
+
+        [PrimaryKey]
+        public Guid MaintEventId { get; set; }
+        
+        public Guid VID { get; set; }
+
+        public String MaintEventTimestamp { get; set; }           // Originally DateTime; Realm doesn't support
+
+        public Double MaintEventMileage { get; set; }
+
+        public String MaintEventName { get; set; }
+
+        public Double Cost { get; set; }
+
+        public String Servicer { get; set; }
+
+        public String Location { get; set; }
+
+        public String Remarks { get; set; }
+
+    }
+}
---
+++ CarLog/ModelsRealm/VehicleRealm.cs
@@ -0,0 +1,37 @@
+﻿using Realms;
+using Realms.Schema;
+using Realms.Weaving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLog.ModelsRealm
+{
+    public partial class VehicleRealm : IRealmObject
+    {
+
+        [PrimaryKey]
+        public Guid VID { get; set; }
+
+        public Int32 VehicleYear { get; set; }
+
+        public String VehicleMake { get; set; }
+
+        public String VehicleModel { get; set; }
+
+        public String VehicleColor { get; set; }
+
+        public Double VehicleMileage { get; set; }
+
+        public String LicensePlateTag { get; set; }
+
+        public String LicensePlateState { get; set; }
+
+        public String LicensePlateExpiry { get; set; }
+
+        public IList<VehicleEventRealm> VehicleEvents { get; }              // Not sure how a 'set' works yet
+
+    }
+}
